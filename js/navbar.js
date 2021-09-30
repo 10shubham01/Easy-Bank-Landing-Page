9fe1b13958eee1576fe827c525cf32cf9c10c211
10Shubham01 <shubham.gupta.17.1@mountblue.tech>
2021-09-30 13:54:51 +0530

    fixed navbar background overlay

--- js/navbar.js
+++ js/navbar.js
@@ -1,29 +1,31 @@
 const navSlide = () => {
-  const links = document.querySelectorAll(".link");
-  links.forEach((btn) => {
-    btn.addEventListener("click", () => {
-      links.forEach((b) => b.classList.remove("active"));
-      btn.classList.add("active");
-      nav.classList.toggle("nav-active");
-      if (nav.classList[1] == "nav-active") {
-        img.src = "./images/icon-close.svg";
-      } else {
-        img.src = "./images/icon-hamburger.svg";
-      }
+    const links = document.querySelectorAll(".link");
+    links.forEach((btn) => {
+        btn.addEventListener("click", () => {
+            links.forEach((b) => b.classList.remove("active"));
+            btn.classList.add("active");
+            nav.classList.toggle("nav-active");
+            if (nav.classList[1] == "nav-active") {
+                img.src = "./images/icon-close.svg";
+            } else {
+                img.src = "./images/icon-hamburger.svg";
+            }
+        });
     });
-  });
 
-  const burger = document.querySelector(".burger");
-  const img = document.querySelector(".img-btn");
-  const nav = document.querySelector(".nav-links");
-
-  burger.addEventListener("click", () => {
-    nav.classList.toggle("nav-active");
-    if (nav.classList[1] == "nav-active") {
-      img.src = "./images/icon-close.svg";
-    } else {
-      img.src = "./images/icon-hamburger.svg";
-    }
-  });
+    const burger = document.querySelector(".burger");
+    const img = document.querySelector(".img-btn");
+    const nav = document.querySelector(".nav-links");
+    const overlay = document.querySelector(".overlay");
+    burger.addEventListener("click", () => {
+        nav.classList.toggle("nav-active");
+        if (nav.classList[1] == "nav-active") {
+            img.src = "./images/icon-close.svg";
+            overlay.style.display = "block";
+        } else {
+            img.src = "./images/icon-hamburger.svg";
+            overlay.style.display = "none";
+        }
+    });
 };
 navSlide();
